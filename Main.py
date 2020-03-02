8c97fa2a0842ac8948fb95c95b4de0351bccad87
irfanisham <irfanisham31@gmail.com>
2020-03-02 16:30:05 +0800

    DA_irfan(22)

--- Main.py
+++ Main.py
@@ -1,26 +1,18 @@
 import numpy as np
+import matplotlib as plt
 import pandas as pd
 
-names = ['Japan', 'Taiwan', 'Hong Kong']
-years = ['2010', '2012', '2014']
-month = ['Jan', 'Feb', 'Mar']
-
-dict = {"country": ["Japan", "Taiwan", "Hongkong"],
-        "years": ["2010", "2012", "2014"],
-        "month" : ["Jan", "Feb", "Mar"]}
-brics = pd.dataframe(dict)
-print(brics)
-
-
-
-
-class calutils :
-    country = []
-    year = []
-    month = []
-
-    class DA :
+class DA:
         def read_data(self):
-            file = "Worksheet in Lesson 2019 Applied Scripting using Python Pyhton Elective (IT49450) - Project_14Mar19.csv"
+            file = "Worksheet in D  Lesson 2019 Applied Sripting Using Python Python Elective (IT49450) - Project_14Mar19.csv"
             df = pd.read_csv(file)
+            print(df)
+
+            print(df.iloc[1:129, 0:18])
+
+
+
+
+country = DA()
+country.read_data()
 
