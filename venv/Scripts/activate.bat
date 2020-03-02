cd68e4b57e40f96556183a3c4856bbbeee7abd4d
irfanisham <irfanisham31@gmail.com>
2020-03-02 15:22:38 +0800

    DA_irfan

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=G:\Pyhton Project\venv"
+set "VIRTUAL_ENV=G:\DA_irfan\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
